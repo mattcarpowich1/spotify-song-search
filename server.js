55ce904a73e5ef4d5e16aa3a8127b902fb26941e
mattcarpowich1 <mattcarpowich@gmail.com>
2018-08-07 18:46:08 -0700

    changed static dir to build

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static(path.join(__dirname, 'client/src/public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', index);
 app.use('/api', api);
